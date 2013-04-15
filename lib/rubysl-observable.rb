a54821a3a9bea5fa8b63fae941f532a534a7884d
Brian Shirai <brixen@gmail.com>
2013-04-14 17:14:51 -0700

    Created gem.

---
+++ lib/rubysl-observable.rb
@@ -0,0 +1,7 @@
+require "rubysl-observable/version"
+
+module RubySL
+  module Observable
+    # Your code goes here...
+  end
+end
---
+++ lib/rubysl-observable/version.rb
@@ -0,0 +1,5 @@
+module RubySL
+  module Observable
+    VERSION = "0.0.1"
+  end
+end
